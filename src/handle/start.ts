b88452f620001497bfd32982784d75309a14b525
OhYoonHee <sudarsana.antara3@gmail.com>
2021-08-23 17:05:45 +0800

    add inline keyboard to start message

--- src/handle/start.ts
+++ src/handle/start.ts
@@ -1,7 +1,10 @@
 import { CustomContext } from "../class/context";
 import Utilities from "../class/utils";
+import { InlineKeyboard } from 'grammy';
 
 export async function StartMessage(ctx : CustomContext) {
+    let keyboard = new InlineKeyboard();
+    keyboard.url('Update Channel', 'https://t.me/TarianaChannel').url('Source', 'https://github.com/OhYoonHee/captcha_bot');
     if( !ctx.from || !ctx.message ) return await Promise.resolve(false);
     let fullname = ctx.from.last_name ? ctx.from.first_name + " " + ctx.from.last_name : ctx.from.first_name;
     let tag = Utilities.create_tag_html(fullname, ctx.from.id);
